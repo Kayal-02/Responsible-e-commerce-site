6e4296630e27396d5761458b53ccba0754eab53b
Kayal-02 <carolinekayal88@gmail.com>
2024-07-30 23:10:23 +0530

    e-commerce site fikes created

---
+++ script.js
@@ -0,0 +1,9 @@
+var sidenavbar = document.querySelector(".side-navbar");
+
+function showmenu() {
+    sidenavbar.style.left = "0";
+}
+
+function closemenu() {
+    sidenavbar.style.left = "-500px";
+}
